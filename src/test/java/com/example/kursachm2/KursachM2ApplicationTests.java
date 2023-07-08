a549fab85ce17e347c802d96806f554c88b09a0e
Dima <dimapanteleyvrach@gmail.com>
2023-07-09 02:55:21 +0300

    ДЗ

---
+++ src/test/java/com/example/kursachm2/KursachM2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursachm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursachM2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
