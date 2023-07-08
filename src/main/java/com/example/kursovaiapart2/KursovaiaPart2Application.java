d232a3d7c19390313fdb8807160f17db1dcb9b02
Dima <dimapanteleyvrach@gmail.com>
2023-07-09 02:28:11 +0300

    дз

---
+++ src/main/java/com/example/kursovaiapart2/KursovaiaPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursovaiapart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovaiaPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursovaiaPart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursovaiapart2/KursovaiaPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovaiapart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovaiaPart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
