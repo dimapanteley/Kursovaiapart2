600d61d988f598be21671aafecd41582d3b0f4e6
Dima <dimapanteleyvrach@gmail.com>
2023-07-09 02:56:14 +0300

    ДЗ

--- src/main/java/com/example/kursachm2/KursachM2Application.java
+++ src/main/java/com/example/kursachm2/KursachM2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KursachM2Application {
+public class  KursachM2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(KursachM2Application.class, args);
