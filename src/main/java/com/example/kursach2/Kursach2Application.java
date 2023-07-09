57d92a4648ad5e36e43d54dcda79473ddf5bb1c8
Dima <dimapanteleyvrach@gmail.com>
2023-07-09 03:14:20 +0300

    первый коммит2

---
+++ src/main/java/com/example/kursach2/Kursach2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursach2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursach2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursach2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursach2/Kursach2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursach2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursach2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
